08ae3eab6f56daada5e6a1a04ffda352327e6dc3
mwyzer <muhammad.wyzer@gmail.com>
2025-06-02 09:34:08 +0700

    break

--- app/Http/Controllers/BookingTransactionController.php
+++ app/Http/Controllers/BookingTransactionController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\TransactionResource;
+use App\Services\BookingTransactionService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class BookingTransactionController extends Controller
 {
     //
+    private $bookingTransactionService;
+
+    public function __construct(BookingTransactionService $bookingTransactionService)
+    {
+        $this->bookingTransactionService = $bookingTransactionService;
+    }
+
+    public function index(Request $request)
+    {
+        $transactions = $this->bookingTransactionService->getAll();
+        return response()->json(TransactionResource::collection($transactions));
+    }
+
+    public function show(int $id)
+    {
+        try {
+            $transaction = $this->bookingTransactionService->getByIdForManager($id);
+            return response()->json(new TransactionResource($transaction));
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['message' => 'Transaction not found'], 404);
+        }
+    }
+
 }
